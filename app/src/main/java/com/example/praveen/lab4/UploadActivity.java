7c547773be3cb73b4e4fc5ba43b6d12817da1f37
Praveen Gupta <praveen@Praveens-MBP.fios-router.home>
2017-05-04 00:59:56 -0400

    Upload working

--- app/src/main/java/com/example/praveen/lab4/UploadActivity.java
+++ app/src/main/java/com/example/praveen/lab4/UploadActivity.java
@@ -1,23 +1,33 @@
 package com.example.praveen.lab4;
 
 import android.app.Activity;
-import android.database.Cursor;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.provider.MediaStore;
-import android.net.Uri;
-import android.widget.ImageView;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.Switch;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnFailureListener;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.UploadTask;
 
 public class UploadActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private static final String TAG = "UploadActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +45,15 @@ public class UploadActivity extends AppCompatActivity {
         mChoose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                uploadPhoto();
+                choosePhoto();
             }
         });
 
+
+
     }
 
-    public void uploadPhoto() {
+    public void choosePhoto() {
         Intent photoPickerIntent = new Intent(
                 Intent.ACTION_PICK,
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
@@ -53,10 +65,10 @@ public class UploadActivity extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == 1)
+        if (requestCode == 1) {
             if (resultCode == Activity.RESULT_OK) {
                 try {
-                    Uri selectedImage = data.getData();
+                    final Uri selectedImage = data.getData();
 
                     String[] filePathColumn = {MediaStore.Images.Media.DATA};
 
@@ -68,19 +80,105 @@ public class UploadActivity extends AppCompatActivity {
                     String picturePath = cursor.getString(columnIndex);
                     cursor.close();
 
-                    ImageView mImageView = (ImageView) findViewById(R.id.imageView);
-
-                    mImageView.setVisibility(View.VISIBLE);
-                    findViewById(R.id.private_switch).setVisibility(View.VISIBLE);
                     findViewById(R.id.upload_button).setVisibility(View.VISIBLE);
+                    findViewById(R.id.imageView).setVisibility(View.VISIBLE);
+
+                    //display private switch only to authenticated users
+                    if (mAuth.getCurrentUser() != null) {
+                        findViewById(R.id.private_switch).setVisibility(View.VISIBLE);
+                    }
+
+                    ImageView mImageView = (ImageView) findViewById(R.id.imageView);
+                    Button mUpload = (Button) findViewById(R.id.upload_button);
+
+                    //activate upload button only if selected image is not blank
+                    if (selectedImage != null && !selectedImage.equals(Uri.EMPTY)) {
+                        mUpload.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                uploadPhoto(selectedImage);
+                            }
+                        });
+                    }else{
+                        Toast.makeText(this, "Choose an Image to Upload", Toast.LENGTH_LONG).show();
+                    }
+
 
                     //display image
                     mImageView.setImageURI(selectedImage);
+
+
+
+
 
                 } catch (Exception e) {
                     Toast.makeText(this, "Enable External Storage Permissions", Toast.LENGTH_LONG).show();
                 }
             }
+        }
+    }
+
+    public void uploadPhoto(Uri selectedImage) {
+        //get description
+        EditText photoDescription = (EditText) findViewById(R.id.editText);
+        String description = photoDescription.getText().toString();
+
+        //set default Privacy Setting to Public
+        Switch privateSwitch = (Switch) findViewById(R.id.private_switch);
+        String privacyState="";
+
+        String imagePath="";
+
+        String userID = "";
+        if(mAuth.getCurrentUser()!=null){
+            userID = mAuth.getCurrentUser().getUid();
+        }
+
+        if(privateSwitch.isChecked()){
+            privacyState="Private";
+            imagePath = "image/private/"+userID+"/"+selectedImage.getLastPathSegment();
+        }else{
+            privacyState = "Public";
+            imagePath = "image/public/"+userID+"/"+selectedImage.getLastPathSegment();
+        }
+
+        if(!description.isEmpty()){
+            // set up firebase storage object
+            FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+
+            // storage reference of firebase
+            StorageReference storageRef = firebaseStorage.getReference();
+
+            // Create a child reference
+            StorageReference pathRef = storageRef.child(imagePath);
+
+            StorageMetadata imageMetadata = new StorageMetadata.Builder()
+                    .setCustomMetadata("UserID", userID)
+                    .setCustomMetadata("PrivacySetting", privacyState)
+                    .setCustomMetadata("Description", description)
+                    .build();
+
+            UploadTask uploadTask = pathRef.putFile(selectedImage, imageMetadata);
+
+            // Register observers to listen for when the download is done or if it fails
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(Exception exception) {
+                    // Handle unsuccessful uploads
+                    Log.e(TAG, "Upload Failed");
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Log.i(TAG, "Upload Successful");
+                    ((Switch) findViewById(R.id.private_switch)).setChecked(false);
+                    ((ImageView) findViewById(R.id.imageView)).setImageResource(0);
+                    ((EditText) findViewById(R.id.editText)).setText("");
+                }
+            });
+
+        }
+
     }
 
 
